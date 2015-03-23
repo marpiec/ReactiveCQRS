f51b7c4be12fd1adb2d635be76cd51da8c658bdc
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-03-23 22:48:05 +0100

    Extracting class from type declaration

--- api/src/main/scala/io/reactivecqrs/api/event/Event.scala
+++ api/src/main/scala/io/reactivecqrs/api/event/Event.scala
@@ -1,12 +1,24 @@
 package io.reactivecqrs.api.event
 
+import java.lang.reflect.Type
+
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl
+
 
 /**
- * This trait marks class as a business event that occured to aggregate.
+ * This trait marks class as a business event that occurred to aggregate.
  * @tparam AGGREGATE type of aggregate this event is related to.
  */
 trait Event[AGGREGATE] {
-  def aggregateType:Class[AGGREGATE] // TODO extract from declaration
+
+  def aggregateType:Class[AGGREGATE] = {
+    var clazz = this.getClass.asInstanceOf[Class[_]]
+    while(clazz.getGenericSuperclass.isInstanceOf[Class[_]]) {
+      clazz = clazz.getGenericSuperclass.asInstanceOf[Class[_]]
+    }
+    val arguments: Array[Type] = clazz.getGenericSuperclass.asInstanceOf[ParameterizedTypeImpl].getActualTypeArguments
+    arguments(0).asInstanceOf[Class[AGGREGATE]]
+  }
 }
 
 /**
