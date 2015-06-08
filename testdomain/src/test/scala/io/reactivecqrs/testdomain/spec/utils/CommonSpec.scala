8e6087469b8d0a1a396c21d6c401f985bec20c95
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-06-08 09:03:19 +0200

    Logging in tests improved

--- testdomain/src/test/scala/io/reactivecqrs/testdomain/spec/utils/CommonSpec.scala
+++ testdomain/src/test/scala/io/reactivecqrs/testdomain/spec/utils/CommonSpec.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{FeatureSpecLike, GivenWhenThen, MustMatchers}
+import scalikejdbc.{LoggingSQLAndTimeSettings, GlobalSettings}
 
 abstract class CommonSpec extends TestKit(ActorSystem("testsystem", ConfigFactory.parseString("""
           akka.loglevel = "DEBUG"
@@ -11,6 +12,17 @@ abstract class CommonSpec extends TestKit(ActorSystem("testsystem", ConfigFactor
           akka.actor.debug.receive = on
           akka.actor.debug.fsm = on
           akka.actor.debug.lifecycle = on"""))) with FeatureSpecLike with GivenWhenThen with ActorAskSupport with MustMatchers {
+
+  GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
+    enabled = true,
+    singleLineMode = true,
+    printUnprocessedStackTrace = false,
+    stackTraceDepth= 1,
+    logLevel = 'debug,
+    warningEnabled = false,
+    warningThresholdMillis = 3000L,
+    warningLogLevel = 'warn
+  )
 
   def step(description: String): Unit = {
     // do nothing
