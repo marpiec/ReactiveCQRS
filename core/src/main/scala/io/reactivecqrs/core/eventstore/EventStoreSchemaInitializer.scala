1ef717cc7de491031c00faf2d5dc9d0d0bf9628d
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-08-21 18:55:12 +0200

    Insert undo event function

--- core/src/main/scala/io/reactivecqrs/core/eventstore/EventStoreSchemaInitializer.scala
+++ core/src/main/scala/io/reactivecqrs/core/eventstore/EventStoreSchemaInitializer.scala
@@ -15,6 +15,7 @@ class EventStoreSchemaInitializer  {
       case e: Exception => () //ignore until CREATE SEQUENCE IF NOT EXISTS is available in PostgreSQL
     }
     createAddEventFunction()
+    createAddUndoEventFunction()
   }
 
 
@@ -85,5 +86,37 @@ class EventStoreSchemaInitializer  {
         """.stripMargin).execute().apply()
   }
 
+  private def createAddUndoEventFunction(): Unit = DB.autoCommit { implicit session =>
+    SQL("""
+          |CREATE OR REPLACE FUNCTION add_undo_event(command_id bigint, user_id bigint, aggregate_id bigint, expected_version INT, aggregate_type VARCHAR(128), event_type VARCHAR(128), event_type_version INT, event VARCHAR(10240))
+          |RETURNS void AS
+          |$$
+          |DECLARE
+          |    current_version int;
+          |BEGIN
+          | SELECT aggregates.version INTO current_version from aggregates where id = aggregate_id;
+          |    IF NOT FOUND THEN
+          |        IF expected_version = 0 THEN
+          |            RAISE EXCEPTION 'Cannot undo event for non existing aggregate';
+          |        ELSE
+          |	    RAISE EXCEPTION 'aggregate not found, id %, aggregate_type %', aggregate_id, aggregate_type;
+          |        END IF;
+          |    END IF;
+          |    IF current_version != expected_version THEN
+          |	RAISE EXCEPTION 'Concurrent aggregate modification exception, command id %, user id %, aggregate id %, expected version %, current_version %, event_type %, event_type_version %, event %', command_id, user_id, aggregate_id, expected_version, current_version, event_type, event_type_version, event;
+          |    END IF;
+          |    INSERT INTO noop_events (id, from_version) (select events.id, current_version + 1
+          |     from events
+          |     left join undos on events.id = undos.id
+          |     where undos.id is null order by events.version desc limit undo_count)
+          |    INSERT INTO noop_events(id, from_version) VALUES (CURRVAL(events_seq), current_version + 1);
+          |    INSERT INTO events (id, command_id, user_id, aggregate_id, event_time, version, event_type, event_type_version, event) VALUES (NEXTVAL('events_seq'), command_id, user_id, aggregate_id, current_timestamp, current_version + 1, event_type, event_type_version, event);
+          |    INSERT INTO events_to_publish (event_id, aggregate_id, version) VALUES(CURRVAL('events_seq'), aggregate_id, current_version + 1);
+          |    UPDATE aggregates SET version = current_version + 1 WHERE id = aggregate_id;
+          |END;
+          |$$
+          |LANGUAGE 'plpgsql' VOLATILE
+        """.stripMargin).execute().apply()
+  }
 
 }
