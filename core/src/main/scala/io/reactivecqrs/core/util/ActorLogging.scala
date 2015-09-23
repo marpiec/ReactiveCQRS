9ffe0fb75f1286e926b8eea05f4e47171628dae6
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-09-23 22:10:19 +0200

    Improved debug logging

--- core/src/main/scala/io/reactivecqrs/core/util/ActorLogging.scala
+++ core/src/main/scala/io/reactivecqrs/core/util/ActorLogging.scala
@@ -1,10 +1,23 @@
 package io.reactivecqrs.core.util
 
 import akka.actor.{Actor, ActorContext}
-import akka.event.LoggingReceive
+import akka.event.{Logging, LoggingReceive}
 
 trait ActorLogging {this: Actor =>
 
-  def logReceive(r: Receive)(implicit context: ActorContext): Receive = LoggingReceive.withLabel("(from " + sender.path.toString+")")(r)
-  
+  val log = Logging(context.system, this)
+
+  def logReceive(r: Receive)(implicit context: ActorContext): Receive = LoggingReceive.withLabel("(from " + sender.path.toString+")")(new Receive {
+    override def isDefinedAt(o: Any): Boolean = r.isDefinedAt(o)
+    override def apply(o: Any): Unit = {
+      try {
+        r(o)
+      } catch {
+        case e: Exception =>
+          log.error(e, s"Error while handling $o from  ${sender()}")
+          throw e;
+      }
+    }
+  })
+
 }
