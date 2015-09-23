a939db7efdb6c7bdb114d35ea0fc869b0f9acac1
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-09-23 22:22:57 +0200

    Logging sender and message in case of exception in actor

--- core/src/main/scala/io/reactivecqrs/core/util/ActorLogging.scala
+++ core/src/main/scala/io/reactivecqrs/core/util/ActorLogging.scala
@@ -3,6 +3,10 @@ package io.reactivecqrs.core.util
 import akka.actor.{Actor, ActorContext}
 import akka.event.{Logging, LoggingReceive}
 
+class ActorException(message: String, cause: Throwable) extends Exception(message, cause) {
+  override def fillInStackTrace(): Throwable = this
+}
+
 trait ActorLogging {this: Actor =>
 
   val log = Logging(context.system, this)
@@ -13,9 +17,7 @@ trait ActorLogging {this: Actor =>
       try {
         r(o)
       } catch {
-        case e: Exception =>
-          log.error(e, s"Error while handling $o from  ${sender()}")
-          throw e;
+        case e: Exception => throw new ActorException(s"Error while handling $o from  ${sender()}", e)
       }
     }
   })
