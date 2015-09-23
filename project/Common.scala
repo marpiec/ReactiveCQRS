452f6352bcfc886affbe1f9e25680498cc878604
Aleksandra Malinowska <aleksandra.barbara.malinowska@gmail.com>
2015-09-23 16:15:07 +0200

    Change version to 0.7.6-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.7.5-SNAPSHOT",
+    version := "0.7.6-SNAPSHOT",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
