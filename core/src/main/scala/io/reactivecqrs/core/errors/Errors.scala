2d13246ed3ecbb8e430d24bd4d1b1c59cc59e6c2
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2016-02-26 22:38:06 +0100

    Cleanup

--- core/src/main/scala/io/reactivecqrs/core/errors/Errors.scala
+++ core/src/main/scala/io/reactivecqrs/core/errors/Errors.scala
@@ -3,4 +3,4 @@ package io.reactivecqrs.core.errors
 import io.reactivecqrs.api.id.AggregateId
 import io.reactivecqrs.api.{AggregateType, AggregateVersion}
 
-case class AggregateConcurrentModificationError(aggregateType: AggregateType, aggregateId: AggregateId, eventTypes: Seq[String],expected: AggregateVersion, was: AggregateVersion)
+case class AggregateConcurrentModificationError(aggregateType: AggregateType, aggregateId: AggregateId, eventTypes: Seq[String], expected: AggregateVersion, was: AggregateVersion)
