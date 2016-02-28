12cdd9e44b6400afd4a4abcc8b2e12a17d36dcc4
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2016-02-28 10:57:06 +0100

    User can create custom responses

--- api/src/main/scala/io/reactivecqrs/api/CommandResponse.scala
+++ api/src/main/scala/io/reactivecqrs/api/CommandResponse.scala
@@ -5,7 +5,7 @@ import _root_.io.reactivecqrs.api.id.AggregateId
 
 // Command handling default response
 
-sealed abstract class CommandResponse
+abstract class CommandResponse
 
 case class SuccessResponse(aggregateId: AggregateId, aggregateVersion: AggregateVersion) extends CommandResponse
 case class FailureResponse(exceptions: List[String]) extends CommandResponse
