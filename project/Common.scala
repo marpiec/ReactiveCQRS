7303fa1e4faf5c7aaabbb2ad2d351825ccbb44e8
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2016-06-10 14:34:40 +0200

    Cleanup

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.8.0-SNAPSHOT",
+    version := "0.8.1-SNAPSHOT",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
