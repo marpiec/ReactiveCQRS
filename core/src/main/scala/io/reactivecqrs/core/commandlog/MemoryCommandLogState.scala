cdc33a809ed4a3f23eb4bc39ac781eea8c6cc4c3
Aleksandra Malinowska <aleksandra.barbara.malinowska@gmail.com>
2016-06-13 15:34:30 +0200

    add implementation to in-memory states (useful for tests)

--- core/src/main/scala/io/reactivecqrs/core/commandlog/MemoryCommandLogState.scala
+++ core/src/main/scala/io/reactivecqrs/core/commandlog/MemoryCommandLogState.scala
@@ -4,9 +4,9 @@ import io.reactivecqrs.api.id.{AggregateId, CommandId}
 
 
 class MemoryCommandLogState extends CommandLogState {
-  override def storeFirstCommand(commandId: CommandId, aggregateId: AggregateId, command: FirstCommand[_, _ <: CustomCommandResponse[_]]): Unit = ???
+  override def storeFirstCommand(commandId: CommandId, aggregateId: AggregateId, command: FirstCommand[_, _ <: CustomCommandResponse[_]]): Unit = ()
 
-  override def storeConcurrentCommand(commandId: CommandId, aggregateId: AggregateId, command: ConcurrentCommand[_, _ <: CustomCommandResponse[_]]): Unit = ???
+  override def storeConcurrentCommand(commandId: CommandId, aggregateId: AggregateId, command: ConcurrentCommand[_, _ <: CustomCommandResponse[_]]): Unit = ()
 
-  override def storeCommand(commandId: CommandId, aggregateId: AggregateId, command: Command[_, _ <: CustomCommandResponse[_]]): Unit = ???
+  override def storeCommand(commandId: CommandId, aggregateId: AggregateId, command: Command[_, _ <: CustomCommandResponse[_]]): Unit = ()
 }
--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.8.1-SNAPSHOT",
+    version := "0.8.2-SNAPSHOT",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
