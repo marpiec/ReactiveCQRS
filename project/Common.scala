94032b50772010a2d978c05376d4e00b5ac93776
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2016-07-26 11:03:30 +0200

    Version incremented

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.9.0-SNAPSHOT",
+    version := "0.9.1-SNAPSHOT",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
