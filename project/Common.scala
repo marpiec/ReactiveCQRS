8581b84ca3213d9e813de9bdb98e67138b749801
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2017-05-16 20:52:36 +0200

    Improved PostgreSQL error handling
    (cherry picked from commit cf0da00)

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.9.20",
+    version := "0.9.21",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
