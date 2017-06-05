8d193d6055482bfb25a32cb32fbb6de73aa20997
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2017-06-05 14:04:14 +0200

    Exception message improved

--- api/src/main/scala/io/reactivecqrs/api/Exceptions.scala
+++ api/src/main/scala/io/reactivecqrs/api/Exceptions.scala
@@ -4,4 +4,4 @@ import io.reactivecqrs.api.id.AggregateId
 
 class NoEventsForAggregateException(aggregateId: AggregateId) extends Exception("No events for aggregate " + aggregateId.asLong)
 
-class AggregateInIncorrectVersionException(aggregateId: AggregateId, currentVersion: AggregateVersion, requestedVersion: AggregateVersion) extends Exception
+class AggregateInIncorrectVersionException(aggregateId: AggregateId, currentVersion: AggregateVersion, requestedVersion: AggregateVersion) extends Exception("Requested version " + requestedVersion.asInt+" but current version is " + currentVersion.asInt)
