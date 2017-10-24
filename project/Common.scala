cc856218974a86300ad2a24512b7c6f3e3691613
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2017-10-24 09:05:25 +0200

    Drop index fix, 0.10.13

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.10.12",
+    version := "0.10.13",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
