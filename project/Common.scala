daaca5dcac7ce22e8a4c5b54eb0f9a3a6f5d9f30
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2018-04-27 15:02:40 +0200

    Projection rebuild logging improved

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.10.17",
+    version := "0.10.18",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
