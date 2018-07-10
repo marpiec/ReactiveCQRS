4ceeaedbec788c20e6cd797a4df91936ef850194
Stanisław Swianiewicz <stanislaw.swianiewicz@gmail.com>
2018-07-10 11:27:09 +0200

    Added support for Postgres 10 by adding fallback query  executed when fetching id sequence step fails.

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.10.20",
+    version := "0.10.21-SNAPSHOT",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
