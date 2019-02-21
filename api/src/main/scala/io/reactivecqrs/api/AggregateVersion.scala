a329d0ce73f3c7defeb615e92dcad6f2c26008c4
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2019-02-21 15:49:45 +0100

    AggregateVersion pooling (memory usage optimization)

--- api/src/main/scala/io/reactivecqrs/api/AggregateVersion.scala
+++ api/src/main/scala/io/reactivecqrs/api/AggregateVersion.scala
@@ -1,23 +1,41 @@
 package io.reactivecqrs.api
 
 object AggregateVersion {
-  val ZERO = AggregateVersion(0)
-  def upTo(version: AggregateVersion, count: Int) = {
+
+  def upTo(version: AggregateVersion, count: Int): List[AggregateVersion] = {
     ((version.asInt - count + 1) to version.asInt).toList.map(i => AggregateVersion(i))
   }
+
+  private final val pool = Range(0, 1000).map(i => new AggregateVersion(i)).toArray
+  val ZERO = pool(0)
+
+  def apply(asInt: Int): AggregateVersion = {
+    if(asInt < 1000) {
+      pool(asInt)
+    } else {
+      new AggregateVersion(asInt)
+    }
+  }
+
+  def unapply(version: AggregateVersion): Option[Int] = Some(version.asInt)
 }
 
-case class AggregateVersion(asInt: Int) {
+class AggregateVersion(val asInt: Int) {
 
-  def < (other: AggregateVersion) = this.asInt < other.asInt
-  def <= (other: AggregateVersion) = this.asInt <= other.asInt
-  def == (other: AggregateVersion) = this.asInt == other.asInt
-  def > (other: AggregateVersion) = this.asInt > other.asInt
-  def >= (other: AggregateVersion) = this.asInt >= other.asInt
+  def < (other: AggregateVersion): Boolean = this.asInt < other.asInt
+  def <= (other: AggregateVersion): Boolean = this.asInt <= other.asInt
+  def == (other: AggregateVersion): Boolean = this.asInt == other.asInt
+  def > (other: AggregateVersion): Boolean = this.asInt > other.asInt
+  def >= (other: AggregateVersion): Boolean = this.asInt >= other.asInt
 
   def increment = AggregateVersion(asInt + 1)
   def incrementBy(count: Int) = AggregateVersion(asInt + count)
 
   def isJustAfter(version: AggregateVersion): Boolean = asInt - 1 == version.asInt
 
+  override def equals(obj: Any): Boolean = obj.isInstanceOf[AggregateVersion] && obj.asInstanceOf[AggregateVersion].asInt == asInt
+
+  override def hashCode(): Int = asInt
+
+  override def toString: String = "Version("+asInt+")"
 }
