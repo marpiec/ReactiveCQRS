53dd0d9a61c4ba7a584bb9eb140a691a5a5a0d34
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2020-01-10 09:00:00 +0100

    Version 0.11.4, logging improved

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.11.3",
+    version := "0.11.4",
     scalaVersion := "2.11.7",
 
     /* required for Scalate to avoid version mismatch */
