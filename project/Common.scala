75d8ac7cc79efe508a4958f0245623f59543fc92
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2024-03-19 12:02:17 +0100

    Version 0.11.43

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common {
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
-    version := "0.11.42",
+    version := "0.11.43",
     scalaVersion := "2.13.11",
 
     /* required for Scalate to avoid version mismatch */
