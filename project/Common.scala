2037559bbcc08c504b816e59222a5b73a84530a2
Marcin Pieciukiewicz <marcin.pieciukiewicz@jtweston.pl>
2025-10-17 19:14:42 +0200

    Logging added, 0.12.35

--- project/Common.scala
+++ project/Common.scala
@@ -5,7 +5,7 @@ object Common {
 
   def settings(moduleName: String) = Seq[Setting[_]](
 
-    version := "0.12.34",
+    version := "0.12.35",
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
