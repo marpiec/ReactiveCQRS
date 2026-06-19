29c8fc5507602cf132f078668a36457cee3eb477
Marcin Pieciukiewicz <marcin.pieciukiewicz@jtweston.pl>
2026-06-19 16:09:34 +0200

    Version 0.12.43

--- project/Common.scala
+++ project/Common.scala
@@ -5,7 +5,7 @@ object Common {
 
   def settings(moduleName: String) = Seq[Setting[_]](
 
-    version := "0.12.42",
+    version := "0.12.43",
 
     organization := "io.reactivecqrs",
     name := s"reactivecqrs-$moduleName",
